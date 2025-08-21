f0575b43a33dd1ee7d9c832997c9ce6e9e07ffd4
joaovmundel <joaovitor20002@gmail.com>
2025-08-21 11:52:22 -0300

    feat: add Angular CDK and Material dependencies, update styles and configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { environment } from '../environments/environment';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideHttpClient(),
-    { provide: AUTH_CONFIG, useValue: { apiUrl: environment.API_URL } },
+    { provide: AUTH_CONFIG, useValue: { apiUrl: environment.API_URL } }, provideAnimationsAsync(),
   ],
 };
