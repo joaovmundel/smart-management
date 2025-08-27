5396357c9405171190e9cd0744ba20cfdd3acf97
joaovmundel <joaovitor20002@gmail.com>
2025-08-27 17:16:48 -0300

    feat(company): implement pagination for company list and enhance company card styling

--- src/libs/admin/src/lib/pages/companies/company.component.ts
+++ src/libs/admin/src/lib/pages/companies/company.component.ts
@@ -1,7 +1,8 @@
-import { Component } from "@angular/core";
-import { Company, mockCompany } from "@smart-management/shared";
-import { CompanyCardComponent } from "../../components/company-card/company-card.component";
 import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { Company, mockCompany, mockCompanyList } from "@smart-management/shared";
+import { CompanyCardComponent } from "../../components/company-card/company-card.component";
 
 @Component({
     selector: 'company-list',
@@ -10,16 +11,36 @@ import { CommonModule } from "@angular/common";
     standalone: true,
     imports: [
         CompanyCardComponent,
-        CommonModule
+        CommonModule,
+        MatButtonModule,
     ]
 })
 export class CompanyComponent {
     companyMock: Company = mockCompany;
-    companyList: Company[] = []
-    
+    companyList: Company[] = mockCompanyList;
+    page = 1;
+    pageSize = 12;
+
     constructor() {
-        for (let i = 0; i < 100; i++) {
-            this.companyList.push(this.companyMock);
+        for (let i = 0; i < 103; i++) {
+            const companyCopy = { ...this.companyMock };
+            companyCopy.name = `${this.companyMock.name} [${i}]`;
+            this.companyList.push(companyCopy);
+        }
+    }
+
+    get paginatedCompanies(): Company[] {
+        const start = (this.page - 1) * this.pageSize;
+        return this.companyList.slice(start, start + this.pageSize);
+    }
+
+    get totalPages(): number {
+        return Math.ceil(this.companyList.length / this.pageSize);
+    }
+
+    setPage(page: number) {
+        if (page >= 1 && page <= this.totalPages) {
+            this.page = page;
         }
     }
 }
--- src/libs/shared/src/lib/mocks/company.mock.ts
+++ src/libs/shared/src/lib/mocks/company.mock.ts
@@ -18,3 +18,43 @@ export const mockCompany: Company = {
     createdAt: new Date('2022-01-01T10:00:00Z'),
     updatedAt: new Date('2023-01-01T10:00:00Z')
 }
+
+export const mockCompanyList: Company[] = [
+    mockCompany,
+    {
+        id: '2',
+        name: 'Tech Innovations Inc.',
+        description: 'Empresa de tecnologia focada em inovações.',
+        logoUrl: 'https://diariodocomercio.com.br/mix/wp-content/uploads/2025/08/nubank-1200x900.png',
+        websiteUrl: 'https://techinnovations.com',
+        cnpj: '98.765.432/0001-10',
+        phone: '+55 11 88888-8888',
+        email: 'contato@techinnovations.com',
+        address: 'Avenida das Tecnologias, 456',
+        city: 'Rio de Janeiro',
+        state: 'RJ',
+        zipCode: '98765-432',
+        country: 'Brasil',
+        isActive: true,
+        createdAt: new Date('2022-02-01T10:00:00Z'),
+        updatedAt: new Date('2023-02-01T10:00:00Z')
+    },
+    {
+        id: '3',
+        name: 'Innovative Tech Solutions',
+        description: 'Empresa de tecnologia focada em soluções inovadoras.',
+        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5e/PicPay_Logogrande.png',
+        websiteUrl: 'https://innovativetech.com',
+        cnpj: '12.345.678/0001-99',
+        phone: '+55 11 99999-9999',
+        email: 'contato@innovativetech.com',
+        address: 'Rua das Inovações, 123',
+        city: 'São Paulo',
+        state: 'SP',
+        zipCode: '01234-567',
+        country: 'Brasil',
+        isActive: true,
+        createdAt: new Date('2022-01-01T10:00:00Z'),
+        updatedAt: new Date('2023-01-01T10:00:00Z')
+    }
+];
