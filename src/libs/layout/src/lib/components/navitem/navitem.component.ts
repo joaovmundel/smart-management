9b8351a4e20a07c8561540a122fd1f39182998df
joaovmundel <joaovitor20002@gmail.com>
2025-09-24 17:17:58 -0300

    feat(navitem): enhance nav item component with icon support and improved layout
    feat(sidebar-menu): add logout item with icon and refactor sidebar structure

--- src/libs/layout/src/lib/components/navitem/navitem.component.ts
+++ src/libs/layout/src/lib/components/navitem/navitem.component.ts
@@ -1,17 +1,21 @@
-import { CommonModule } from "@angular/common";
-import { Component, Input } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { NavItem } from "../../models/navigation.model";
-import { MatButtonModule } from "@angular/material/button";
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { NavItem } from '../../models/navigation.model';
 
 @Component({
-    selector: "lib-navitem",
-    templateUrl: "./navitem.component.html",
-    styleUrls: ["./navitem.component.scss"],
-    standalone: true,
-    imports: [CommonModule, RouterModule, MatButtonModule]
+  selector: 'lib-navitem',
+  templateUrl: './navitem.component.html',
+  styleUrls: ['./navitem.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule],
 })
 export class NavItemComponent {
-    @Input() isSuspended = false;
-    @Input() item!: NavItem;
+  @Input() isSuspended = false;
+  @Input() item!: NavItem;
+  @Input() hasIcon = false;
+  @Input() iconName = '';
+  @Input() iconPosition: 'start' | 'end' = 'start';
 }
--- src/libs/layout/src/lib/components/sidebar-menu/sidebar-menu.component.ts
+++ src/libs/layout/src/lib/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,19 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { navbarRoutes } from '../../config/navbar-routes.config';
 import { DropdownItem, NavItem } from '../../models/navigation.model';
 import { NavItemComponent } from '../navitem/navitem.component';
 import { DropdownComponent } from '../dropdown/dropdown.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sm-sidebar-menu',
   templateUrl: './sidebar-menu.component.html',
   styleUrls: ['./sidebar-menu.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, NavItemComponent, DropdownComponent],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    NavItemComponent,
+    DropdownComponent,
+  ],
 })
 export class SidebarMenuComponent {
   @Input() isOpen = true;
+
   navItems: (NavItem | DropdownItem)[] = navbarRoutes;
+  private readonly _route = inject(Router);
+
+  goHome(): void {
+    this._route.navigateByUrl('/home');
+  }
 }
