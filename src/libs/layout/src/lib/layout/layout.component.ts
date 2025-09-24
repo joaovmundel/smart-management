47887b0c1fffc5beeeeb2bf903323b79b5d22963
joaovmundel <joaovitor20002@gmail.com>
2025-09-24 17:43:54 -0300

    feat(new-header): add new header component with styles and integration

--- src/libs/layout/src/lib/layout/layout.component.ts
+++ src/libs/layout/src/lib/layout/layout.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
 import { SidebarMenuComponent } from '../components/sidebar-menu/sidebar-menu.component';
+import { NewHeaderComponent } from '../components/new-header/new-header.component';
 
 @Component({
   selector: 'lib-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent, SidebarMenuComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent, SidebarMenuComponent, NewHeaderComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
